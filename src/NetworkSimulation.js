4742e12ed007a151e8f93f28d0a54969070a817a
Xmonad Jump Virtual Box <xmonad-jumpbox@trackingdesk.com>
2018-01-04 23:55:04 +0000

    balagan with async again

--- src/NetworkSimulation.js
+++ src/NetworkSimulation.js
@@ -1,23 +1,63 @@
-let filename = __filename.split('/');
-filename = filename[filename.length-1].split('.');
+const Async = require("async");
+let filename = __filename.split("/");
+filename = filename[filename.length - 1].split(".");
 filename = filename[0];
-const index  = require('./index');
+const index = require("./index");
 let indexMod = index();
 let debug = require("debug")(filename + "-dbg");
 debug("hello");
-module.exports = function () {
+module.exports = function() {
   let testNet = indexMod.getTestNetworkIds();
   function createNodes(nodesMeta) {
     let nodes = [];
     for (var i = 0, len = nodesMeta.length; i < len; i++) {
-      let node = indexMod.createNode(nodesMeta[i])
+      let node = indexMod.createNode(nodesMeta[i]);
       nodes.push(node);
     }
     return nodes;
   }
   return {
-    seed : indexMod.createNode(testNet.seeds[0]),
-    nodes: createNodes(testNet.nodes)
-  }
-}
-
+    seed: indexMod.createNode(testNet.seeds[0]),
+    nodes: createNodes(testNet.nodes),
+    joinNodes: function(next) {
+      let self = this;
+      let seedMeta = testNet.seeds[0];
+      seedMeta = [seedMeta[0], {hostname: seedMeta[1], port: seedMeta[2]}]
+      Async.mapSeries(
+        self.nodes,
+        function(instance, callback) {
+          //callback(null, instance)
+          debug(seedMeta)
+          instance.join(seedMeta, function() {
+              if (instance.router.size) {
+                debug(`Connected to ${instance.router.size} peers!`);
+                callback(null, instance.router.size);
+              } else {
+                debug("No seed or peers found.");
+                callback(null, new Error("Not able to connect to any peer"));
+              }
+            });
+        },
+        next
+      );
+    },
+    stopNodes: function(next) {
+      Async.mapSeries(
+        this.nodes,
+        function(instance, callback) {
+          instance.transport.server
+            .close() // Won't accept new connection
+            .on("close", () => {
+              debug(" Server stopped");
+              callback(null);
+            })
+            .on("error", e => {
+              debug(" Error stopping server:", e);
+              callback(null,e);
+            });
+        },
+        next
+      );
+    }
+  };
+};
--- test/NetworkSimulation.spec.js
+++ test/NetworkSimulation.spec.js
@@ -2,6 +2,7 @@ let chai = require('chai');
 let expect = chai.expect;
 
 let NetworkSimulation = require('../src/NetworkSimulation')
+let debug = require("debug")('NetSimTest' + "-dbg");
 
 describe('"NetworkSimulation" module exports', function(){
   let simulation = NetworkSimulation();
@@ -10,5 +11,15 @@ describe('"NetworkSimulation" module exports', function(){
   })
   it('should return an instance of "Object".', function() {
     expect(simulation).to.be.instanceOf(Object)
+    expect(simulation.nodes).to.be.instanceOf(Array)
+    expect(simulation.seed).to.be.instanceOf(Object)
+    expect(simulation.joinNodes).to.be.instanceOf(Function)
+    expect(simulation.stopNodes).to.be.instanceOf(Function)
   })
+  it('should be able to join all nodes in test net.', function(done) {
+    simulation.joinNodes(function (err, result) {
+      debug(result)
+      done();
+    })
+  });
 })
